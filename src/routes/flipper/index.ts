6cd778f016b6766a3102cd26270f5e97f92999f8
Dani Verhaar <daniverhaar@MacBook-Pro-FVFF94UNQ05N.local>
2021-10-03 14:48:20 +0200

    Added auctions

--- src/routes/flipper/index.ts
+++ src/routes/flipper/index.ts
@@ -3,9 +3,18 @@ import database from '../../../database';
 
 const router = express.Router();
 
-router.get('/flips', async (req: Request, res: Response) => {
+router.get('/bins', async (req: Request, res: Response) => {
     res.setHeader('Content-Type', 'application/json');
-    const auctions = await database('auction').orderBy('updated_at', 'desc').orderBy('start', 'desc').limit(20);
+    const auctions = await database('bins').orderBy('updated_at', 'desc').orderBy('start', 'desc').limit(20);
+
+    res.status(200).json({
+        auctions
+    });
+});
+
+router.get('/auctions', async (req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
+    const auctions = await database('auctions').orderBy('updated_at', 'desc').orderBy('start', 'desc').limit(20);
 
     res.status(200).json({
         auctions
